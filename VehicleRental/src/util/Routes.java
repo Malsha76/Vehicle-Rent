9378e67a3d0e309c814e5636389860cc0fe68c90
Malsha76 <tharu590ekanayake@gmail.com>
2023-01-27 15:53:54 +0530

    add utill

---
+++ VehicleRental/src/util/Routes.java
@@ -0,0 +1,13 @@
+package util;
+
+public enum Routes {
+    LOGIN,
+    BOOKING,
+    MANAGE_CUSTOMER,
+    MANAGE_EMPLOYEE,
+    MANAGE_VEHICLES,
+    PAYMENT,
+    REPORTS,
+    PACKAGES,
+    DASHBOARD;
+}
